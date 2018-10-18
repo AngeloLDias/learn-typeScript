d57ed6915edbc9ccdd0967bb83a2b34b8e556613
Angelo Lima Dias <angelolimadias@gmail.com>
2018-10-18 17:46:16 -0300

    compilar e variaveis

---
+++ typeScript/app.ts
@@ -0,0 +1,12 @@
+let message: string = "hello world"
+console.log(message)
+
+let episode: number = 4
+// episode = '7' erro
+console.log("this is episode" + 4)
+episode = episode +1
+console.log("next episode is" + episode) 
+
+let favoirteDroid: string;
+favoirteDroid = "BB-8"
+console.log('My favorite droid is ' + favoirteDroid)
---
+++ typeScript/dist/app.js
@@ -0,0 +1,10 @@
+var message = "hello world";
+console.log(message);
+var episode = 4;
+// episode = '7' erro
+console.log("this is episode" + 4);
+episode = episode + 1;
+console.log("next episode is" + episode);
+var favoirteDroid;
+favoirteDroid = "BB-8";
+console.log('My favorite droid is ' + favoirteDroid);
